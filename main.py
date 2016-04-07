489cc1e552a60fa91ceff00e10fc7ca3478942b7
mozillazg <opensource.mozillazg@gmail.com>
2016-04-08 07:58:26 +0800

    PYTHONPATH include old value

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ current_dir = os.path.dirname(os.path.realpath(__file__))
 
 def main():
     args = sys.argv[2:] if len(sys.argv) > 2 else []
-    os.environ['PYTHONPATH'] = os.path.join(current_dir, 'bootstrap')
+    os.environ['PYTHONPATH'] = '{}:{}'.format(
+        os.path.join(current_dir, 'bootstrap'),
+        os.environ.get('PYTHONPATH')
+    )
     os.execl(sys.executable, sys.executable, *args)
 
 if __name__ == '__main__':
